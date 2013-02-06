7982a971f2fdd48cd7903c8595609a5172f0ed48
David Caro <dcaroest@redhat.com>
2013-02-06 21:01:38 +0100

    Improved the code and changed the behavior and done some tests (and bugs fixed)
     - Only managed hosts are taken into account

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get "api/hosts_reserve" => "foreman_reserve/hosts#reserve"
-  get "api/hosts_release" => "foreman_reserve/hosts#release"
-  get "api/show_released" => "foreman_reserve/hosts#show_released"
-  get "api/show_reserved" => "foreman_reserve/hosts#show_reserved"
+  get "api/hosts_reserve"          => "foreman_reserve/hosts#reserve"
+  get "api/hosts_release"          => "foreman_reserve/hosts#release"
+  get "api/show_available"    	   => "foreman_reserve/hosts#show_available"
+  get "api/show_reserved"          => "foreman_reserve/hosts#show_reserved"
   get "api/update_reserved_reason" => "foreman_reserve/hosts#update_reason"
 
 end
